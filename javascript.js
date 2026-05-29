dd8000c13b8c48f390766ab772677033973fb8fd
NicoQDS <nico.osnaghi@gmail.com>
2026-05-29 09:00:55 +0200

    Created the function to build the divs array.
    The array has the same dimension as the input parameter.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,28 @@
-// let variable = 0
+let dimGrid = 16
+let rowDivs = new Array();
+let arrDivs = new Array();
+const gridContainer = document.querySelector("#grid-container");
 
-// function myFunction() {
-// }
+
+
+// Function to populate the div-container with divs
+function createGrid(num){
+    rowDivs = []
+    arrDivs = []
+    // Creating an array containing the number of divs according to the parameter received
+    for (let i = 0; i < num; i++) { 
+        for (let j = 0; j < num; j++){
+            rowDivs.push(document.createElement("div"))
+        }
+        arrDivs.push(rowDivs)
+        rowDivs = []
+    }
+
+    // Lets populate #grid-container
+    while(true){
+        // do something
+    }
+}
+
+// Running the function
+createGrid(dimGrid)
